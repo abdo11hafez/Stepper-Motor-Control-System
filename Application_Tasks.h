f20ef4639b5920c5f2b6122b702eb7f7ae9ff79c
Abdelrahman Mohamed Ali <47254796+abdo11hafez@users.noreply.github.com>
2019-12-20 22:38:59 +0200

    Update Application_Tasks.h

--- Application_Tasks.h
+++ Application_Tasks.h
@@ -7,21 +7,22 @@
 
 
 #ifndef APPLICATION_TASKS_H_
-#define APPLICATION_TASKS_H_
+ #define APPLICATION_TASKS_H_
  #include "FreeRTOS.h"
  #include "task.h"
  #include "queue.h"
  #include <stdint.h>
  #include "GPIO.h"
+ #include <avr/interrupt.h>
  
  typedef struct
  {
 	 QueueHandle_t	Speed_Q;
 	 QueueHandle_t  Direction_Q	;
- } SD_Queue_Struct;
+	 QueueHandle_t  Step_Q;
+ } Parameters_Queue_Struct;
  
- void Speed_Button_Task(void * Speed_Queue);
- void Direction_Button_Task(void * Direction_Queue);
+ void Interrupt_Init(void);
  void Stepper_Motor_Task(void * S_D_Queue);
 
 #endif /* APPLICATION_TASKS_H_ */
